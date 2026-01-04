9d755eef203c6e11f013bb3dcca95ec78dc9fc7a
정지윤 <163826760+jiyun040@users.noreply.github.com>
2026-01-04 22:51:38 +0900

    feat: add  TextStyle and Color definitions

---
+++ lib/core/constants/color.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/painting.dart';
+
+abstract final class FalletterColor {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color background = Color(0xFFF4F4F4);
+  static const Color middleBlack = Color(0xFFECECEC);
+
+  static const Color red = Color(0xFFFF6868);
+  static const Color blue = Color(0xFF68A4FF);
+  static const Color yellow = Color(0xFFF8C703);
+
+  static const Color gray50  = Color(0xFFEDEDED);
+  static const Color gray100 = Color(0xFFEAEAEA);
+  static const Color gray200 = Color(0xFFE8E8E8);
+  static const Color gray300 = Color(0xFFE5E5E5);
+  static const Color gray400 = Color(0xFFC6C6C6);
+  static const Color gray500 = Color(0xFFBABABA);
+  static const Color gray600 = Color(0xFFA7A7A7);
+  static const Color gray700 = Color(0xFF939393);
+  static const Color gray800 = Color(0xFF6A6A6A);
+  static const Color gray900 = Color(0xFF5C5C5C);
+}
