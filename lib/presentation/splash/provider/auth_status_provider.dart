54ab57cd0e6b84a8b9c71b36f9855f3352468b2c
정지윤 <163826760+jiyun040@users.noreply.github.com>
2026-01-06 15:14:16 +0900

    fix: correct incorrectly written code

--- lib/presentation/splash/provider/auth_status_provider.dart
+++ lib/presentation/splash/provider/auth_status_provider.dart
@@ -9,7 +9,7 @@ final authStatusProvider = FutureProvider<AuthStatus>((ref) async {
   await Future.delayed(const Duration(seconds: 1));
 
   /// TODO: secure storage / prefs 에서 토큰 확인
-  final hasToken = true;
+  final hasToken = false;
 
   if(hasToken) {
     return AuthStatus.logIn;
